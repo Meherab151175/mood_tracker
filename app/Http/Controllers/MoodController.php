c896cd14c2178e5f650b8b71c332a3e434c92ce0
Meherab Hossain <meherabm803@gmail.com>
2025-06-30 15:05:19 +0600

    update:readme.MD

--- app/Http/Controllers/MoodController.php
+++ app/Http/Controllers/MoodController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use App\Models\Mood;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
